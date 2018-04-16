39c9ca0bea8fe663010ffb66faffcfe18b66c048
Dulce Russo <ai.angel_@hotmail.com>
2018-04-15 20:57:48 -0500

    TodoInput listo
    Me falta el otro componente

---
+++ resources/assets/js/components/TodoInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="box">
+        <div class="field is-grouped">
+            <p class="control is-expanded">
+                <input class="input"
+                       type="text"
+                       placeholder="Nuevo recordatorio"
+                       :value="todoItemText"
+                       @input="$emit('update:todoItemText', $event.target.value)"
+                >
+            </p>
+            <p class="control">
+                <a class="button is-info" @click="$emit('addTodo')">
+                    Agregar
+                </a>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['todoItemText']
+    }
+</script>
+
+<style scoped>
+
+</style>
